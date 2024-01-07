0bd22f86e32edbde6015d229f755f9b4ca1f50c6
Hubert Rutkowski <hubr2000@gmail.com>
2024-01-07 20:02:12 +0100

    added entities into prod orm config

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -1,5 +1,10 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from 'src/modules/auth/entities/user.entity';
+import { Access } from 'src/modules/accesses/entities/access.entity';
+import { Set } from 'src/modules/sets/entities/set.entity';
+import { Word } from 'src/modules/sets/entities/word.entity';
+import { UserWordLvl } from 'src/modules/sets/entities/userWordLvl.entity';
 
 export default registerAs(
   'orm.config',
@@ -10,7 +15,7 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [],
+    entities: [User, Set, Access, Word, UserWordLvl],
     synchronize: false,
   }),
 );
